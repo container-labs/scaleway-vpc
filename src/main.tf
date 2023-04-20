6aea23e936ad0d4337e11208fc11d2aca3167f96
Will <will@containerlabs.io>
2023-04-20 04:43:04 -0700

    fmt

--- src/main.tf
+++ src/main.tf
@@ -1,7 +1,7 @@
 module "virtual_network" {
-  source = "container-labs/vpc/scaleway"
+  source  = "container-labs/vpc/scaleway"
   version = "~> 0.2"
-  name   = var.md_metadata.name_prefix
-  tags   = var.md_metadata.default_tags
-  region = var.region
+  name    = var.md_metadata.name_prefix
+  tags    = var.md_metadata.default_tags
+  region  = var.region
 }
